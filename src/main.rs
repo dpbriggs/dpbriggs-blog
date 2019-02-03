3220e6ef182f82d68ea9da6d1c2179e292d2a7d8
David Briggs <dpbriggs@edu.uwaterloo.ca>
2019-02-03 12:59:37 -0500

    Add site styling, index content, 404/500 handling

--- src/main.rs
+++ src/main.rs
@@ -4,36 +4,29 @@
 #[macro_use]
 extern crate rocket;
 extern crate rocket_contrib;
+use rocket::http::Status;
+use rocket::Request;
 use rocket_contrib::serve::StaticFiles;
 use rocket_contrib::templates::Template;
 
-#[macro_use]
-extern crate serde_derive;
-extern crate serde;
-extern crate serde_json;
-
 use std::collections::HashMap;
 
-type Foo = HashMap<String, String>;
+type SiteContext = HashMap<String, String>;
 
-#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
-struct FooBar {
-    foo: u64,
-    bar: String,
-}
-
-#[get("/foobar/<age>/<name>")]
-fn foobar(age: u64, name: String) -> String {
-    let f = FooBar {
-        foo: age,
-        bar: name,
-    };
-    serde_json::to_string_pretty(&f).unwrap()
-}
-
-fn get_base_context() -> Foo {
-    let mut context = Foo::new();
-    context.insert("site_name".to_owned(), "dpbriggs".to_owned());
+fn get_base_context() -> SiteContext {
+    let mut context = SiteContext::new();
+    context.insert("nav_site_name".to_owned(), "dpbriggs.ca".to_owned());
+    context.insert("nav_site_href".to_owned(), "/".to_owned());
+    context.insert("root_uri".to_owned(), "/".to_owned());
+    context.insert("blog_uri".to_owned(), "/blog".to_owned());
+    context.insert("resume_uri".to_owned(), "/resume".to_owned());
+    context.insert("crash_uri".to_owned(), "/500".to_owned());
+    context.insert("admin_email".to_owned(), "email@dpbriggs.ca".to_owned());
+    context.insert("my_email".to_owned(), "email@dpbriggs.ca".to_owned());
+    context.insert(
+        "github_url".to_owned(),
+        "https://github.com/dpbriggs".to_owned(),
+    );
     context
 }
 
@@ -43,12 +36,32 @@ fn index() -> Template {
     Template::render("index", context)
 }
 
+#[get("/500")]
+fn crash() -> Result<String, Status> {
+    Err(Status::InternalServerError)
+}
+
+#[catch(404)]
+fn not_found(req: &Request) -> Template {
+    let mut context = get_base_context();
+    context.insert("uri".to_owned(), req.uri().to_string());
+    Template::render("404", context)
+}
+
+#[catch(500)]
+fn server_err(req: &Request) -> Template {
+    let mut context = get_base_context();
+    context.insert("uri".to_owned(), req.uri().to_string());
+    Template::render("500", context)
+}
+
 fn main() {
     let static_files = StaticFiles::from("static");
-    let routes = routes![foobar, index];
+    let routes = routes![index, crash];
     rocket::ignite()
         .mount("/", routes)
         .mount("/static", static_files)
+        .register(catchers![not_found, server_err])
         .attach(Template::fairing())
         .launch();
 }
